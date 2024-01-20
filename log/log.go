318a6556aa95e09b4637f482e3f25d902f50c3ba
Tess Ellenoir Duursma <tess@albird.nl>
2024-01-20 01:02:29 +0100

    Add support for multiple inputs to the log screen

--- log/log.go
+++ log/log.go
@@ -1,24 +1,63 @@
 package log
 
 import (
+	"fmt"
+
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
-	doseInput     textinput.Model
-	returnToStart func() tea.Model
+	activeInputIndex int
+	inputs           []textinput.Model
+	returnToStart    func() tea.Model
 }
 
+type field struct {
+	label       string
+	placeholder string
+}
+
+var fields = [3]field{
+	{
+		label:       "Dose",
+		placeholder: "1,5 mg",
+	},
+	{
+		label:       "Substance",
+		placeholder: "Estradiol",
+	},
+	{
+		label:       "Route",
+		placeholder: "Transdermal",
+	},
+}
+
+const (
+	DOSE_INDEX int = iota
+	SUBSTANCE_INDEX
+	ROUTE_INDEX
+)
+
 func InitializeModel(returnToStart func() tea.Model) tea.Model {
-	doseInput := textinput.New()
-	doseInput.Placeholder = "1,5 mg"
-	doseInput.Focus()
-	doseInput.Width = 20
+	var activeInputIndex int
+
+	inputs := make([]textinput.Model, 3)
+
+	for i := range inputs {
+		inputs[i] = textinput.New()
+		inputs[i].Width = 20
+		inputs[i].Prompt = fmt.Sprintf("%s:\n", fields[i].label)
+		//inputs[i].PromptStyle.Align(lipgloss.Center)
+		inputs[i].Placeholder = fields[i].placeholder
+	}
+
+	inputs[0].Focus()
 
 	return model{
-		doseInput,
+		activeInputIndex,
+		inputs,
 		returnToStart,
 	}
 }
@@ -33,26 +72,42 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+c":
 			return m, tea.Quit
+		case "tab":
+			if m.activeInputIndex >= 0 {
+				m.inputs[m.activeInputIndex].Blur()
+			}
+
+			m.activeInputIndex++
+
+			if m.activeInputIndex >= len(m.inputs) {
+				m.activeInputIndex = 0
+			}
+
+			m.inputs[m.activeInputIndex].Focus()
 		default:
-			if m.doseInput.Focused() {
-				switch msg.String() {
-				case "esc":
-					m.doseInput.Blur()
-				}
-			} else {
+			if m.activeInputIndex == -1 {
 				switch msg.String() {
 				case "q", "esc":
 					return m.returnToStart(), nil
+				}
+			} else {
+				switch msg.String() {
+				case "esc":
+					m.inputs[m.activeInputIndex].Blur()
+
+					m.activeInputIndex = -1
 				}
 			}
 		}
 	}
 
-	var cmd tea.Cmd
+	cmds := make([]tea.Cmd, len(m.inputs))
 
-	m.doseInput, cmd = m.doseInput.Update(msg)
+	for i := 0; i < len(m.inputs); i++ {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
 
-	return m, cmd
+	return m, tea.Batch(cmds...)
 }
 
 var inputStyle = lipgloss.NewStyle().
@@ -61,9 +116,10 @@ var inputStyle = lipgloss.NewStyle().
 	Width(25).Align(lipgloss.Center)
 
 func (m model) View() string {
-	renderedDoseInput := inputStyle.Render("Dose:\n" + m.doseInput.View())
-	renderedSubstanceInput := inputStyle.Render("Substance:\n" + m.doseInput.View())
-	renderedRouteInput := inputStyle.Render("Route:\n" + m.doseInput.View())
+	renderedFields := make([]string, len(fields))
+	for i := 0; i < len(fields); i++ {
+		renderedFields[i] = inputStyle.Render(m.inputs[i].View())
+	}
 
-	return lipgloss.JoinHorizontal(lipgloss.Top, renderedDoseInput, renderedSubstanceInput, renderedRouteInput)
+	return lipgloss.JoinHorizontal(lipgloss.Top, renderedFields...)
 }
