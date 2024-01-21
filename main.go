d92221c10e1729179a2a0dc84cf421259bf5c814
Tess Ellenoir Duursma <tess@albird.nl>
2024-01-21 20:18:43 +0100

    Add debugging

--- main.go
+++ main.go
@@ -10,6 +10,15 @@ import (
 )
 
 func main() {
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
 	program := tea.NewProgram(startMenu.InitializeModel())
 
 	if _, err := program.Run(); err != nil {
