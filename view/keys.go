07a172168e52e26f540fa1c14c2c08ad8b8d06ea
Tess Ellenoir Duursma <tess@vanu.dev>
2024-02-28 17:18:03 +0100

    Change the help section's layout

--- view/keys.go
+++ view/keys.go
@@ -18,8 +18,8 @@ func (k keyMap) ShortHelp() []key.Binding {
 func (k keyMap) FullHelp() [][]key.Binding {
 	return [][]key.Binding{
 		{k.Up, k.Down},
-		k.ShortHelp(),
-		{k.Exit},
+		{k.Delete, k.Help},
+		{k.Quit, k.Exit},
 	}
 }
 
