fda3913d4b8e205a0755399b81ee443c6b0551c5
Tess Ellenoir Duursma <tess@vanu.dev>
2024-03-14 23:53:03 +0100

    Add support for d-h-m notation

--- log/popup.go
+++ log/popup.go
@@ -8,6 +8,58 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+func increment(raw_before_delimiter *string, field *int) error {
+	val, err := strconv.Atoi(*raw_before_delimiter)
+	*raw_before_delimiter = ""
+
+	if err == nil {
+		*field += val
+	}
+
+	return err
+}
+
+func parseTime(raw string) (int, error) {
+	days := 0
+	hours := 0
+	minutes := 0
+
+	raw_before_delimiter := ""
+
+	for i := range raw {
+		r := raw[i]
+		var err error
+
+		switch r {
+		case 'd':
+			err = increment(&raw_before_delimiter, &days)
+		case 'h':
+			err = increment(&raw_before_delimiter, &hours)
+		case 'm':
+			err = increment(&raw_before_delimiter, &minutes)
+		default:
+			raw_before_delimiter += string(r)
+		}
+
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	if len(raw_before_delimiter) > 0 {
+		err := increment(&raw_before_delimiter, &minutes)
+
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	hours += days * 24
+	minutes += hours * 60
+
+	return minutes, nil
+}
+
 type popupModel struct {
 	input textinput.Model
 }
@@ -34,7 +86,7 @@ func (m popupModel) Update(msg tea.Msg) (*int, *popupModel, tea.Cmd) {
 			return nil, &m, tea.Quit
 		case "enter":
 			raw_value := m.input.Value()
-			value, err := strconv.Atoi(raw_value)
+			value, err := parseTime(raw_value)
 
 			if err != nil {
 				m.input.Prompt = "Minutes since dose:\n(Must be an integer)\n"
