3733aa5dd97bce15dd83101aab7304e6d59c64d7
Freya Arbjerg <freya@arbjerg.dev>
2024-04-17 01:06:57 +0200

    Implement ID migration in encoding.go
    Co-authored-by: Tess Ellenoir Duursma <tess@vanu.dev>

--- data/fs/encoding.go
+++ data/fs/encoding.go
@@ -6,6 +6,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	gonanoid "github.com/matoous/go-nanoid/v2"
 )
 
 func escape(raw string) string {
@@ -16,16 +18,18 @@ func escape(raw string) string {
 }
 
 func encode(d data.Dose) string {
-	return fmt.Sprintf("%s;%s;%s;%s;", escape(strconv.FormatInt(d.Time.Unix(), 10)), escape(d.Quantity), escape(d.Substance), escape(d.Route))
+	return fmt.Sprintf("%s;%s;%s;%s;%s;", escape(string(d.Id)), escape(strconv.FormatInt(d.Time.Unix(), 10)), escape(d.Quantity), escape(d.Substance), escape(d.Route))
 }
 
 const (
 	ENDS_WITH_ESCAPE = iota
 	BAD_TIME
+	UNKNOWN_HEADER
 )
 
 type DecodeError struct {
-	Kind int
+	Kind    int
+	context *string
 }
 
 func (e DecodeError) Error() string {
@@ -34,19 +38,23 @@ func (e DecodeError) Error() string {
 		return "The raw string ends with an escape character."
 	case BAD_TIME:
 		return "Failed to parse a time stamp."
+	case UNKNOWN_HEADER:
+		return fmt.Sprintf("Unknown header encountered when reading file: %s", *e.context)
 	default:
 		panic(fmt.Sprintf("DecodeError.Error has no case for error code %d.", e.Kind))
 	}
 }
 
-func decode(raw string) ([]data.Dose, error) {
+type fieldParser func([]string) (*data.Dose, error)
+
+func parse(raw string, fieldCount int, parseFields fieldParser) ([]data.Dose, error) {
 	runes := []rune(raw)
 	doses := make([]data.Dose, 0)
 
 	for j := 0; j < len(runes); {
-		sections := [5]string{"", "", "", "", ""}
+		sections := make([]string, fieldCount)
 	sectionLoop:
-		for i := 0; i < 5; i++ {
+		for i := 0; i < fieldCount; i++ {
 			for ; j < len(runes); j++ {
 				character := runes[j]
 
@@ -57,6 +65,7 @@ func decode(raw string) ([]data.Dose, error) {
 					}
 				} else if character == ';' {
 					j++
+
 					continue sectionLoop
 				}
 
@@ -68,21 +77,66 @@ func decode(raw string) ([]data.Dose, error) {
 			j++
 		}
 
+		dose, err := parseFields(sections)
+
+		if err != nil {
+			return nil, err
+		} else {
+			doses = append(doses, *dose)
+		}
+	}
+
+	return doses, nil
+}
+
+func parseHeaderless(raw string) ([]data.Dose, error) {
+	return parse(raw, 4, func(sections []string) (*data.Dose, error) {
 		unix, err := strconv.ParseInt(sections[0], 10, 64)
 
 		if err != nil {
 			return nil, DecodeError{Kind: BAD_TIME}
 		}
 
-		doses = append(doses,
-			data.Dose{
-				Id:        data.Id(sections[0]),
-				Time:      time.Unix(unix, 0),
-				Quantity:  sections[1],
-				Substance: sections[2],
-				Route:     sections[3],
-			})
-	}
+		id, err := gonanoid.New()
 
-	return doses, nil
+		if err != nil {
+			return nil, err
+		}
+
+		return &data.Dose{
+			Id:        data.Id(id),
+			Time:      time.Unix(unix, 0),
+			Quantity:  sections[1],
+			Substance: sections[2],
+			Route:     sections[3],
+		}, nil
+	})
+}
+
+func parseVersion1(raw string) ([]data.Dose, error) {
+	return parse(raw, 5, func(sections []string) (*data.Dose, error) {
+		unix, err := strconv.ParseInt(sections[1], 10, 64)
+
+		if err != nil {
+			return nil, DecodeError{Kind: BAD_TIME}
+		}
+
+		return &data.Dose{
+			Id:        data.Id(sections[0]),
+			Time:      time.Unix(unix, 0),
+			Quantity:  sections[2],
+			Substance: sections[3],
+			Route:     sections[4],
+		}, nil
+	})
+}
+
+func decode(raw string) ([]data.Dose, error) {
+	if !strings.HasPrefix(raw, "Version:") {
+		return parseHeaderless(raw)
+	} else if strings.HasPrefix("Version:1\n", raw) {
+		return parseVersion1(strings.TrimPrefix(raw, "Version:1\n"))
+	} else {
+		return nil, DecodeError{Kind: UNKNOWN_HEADER, context: &strings.SplitN(raw, "\n", 1)[0]}
+	}
 }
